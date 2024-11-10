491f3fb46f697f063ef6eb0a1f5e69428e314760
Skyteks <Schattenpalkia@googlemail.com>
2024-11-10 22:03:06 +0100

    Fix note having transition

--- src/components/PinnedNote.jsx
+++ src/components/PinnedNote.jsx
@@ -44,7 +44,7 @@ function PinnedNote({ entry, handleDrag }) {
         <div style={{ width: "300px", height: "300px", display: "flex", justifyContent: "center", alignItems: "center", }}>
             <div style={{ opacity: (dragging ? 0.2 : 1), width: "80%", height: "80%", rotate: angle + "deg", display: "flex", alignItems: "center", flexDirection: "column", boxShadow: "rgba(0,0,0,0.5) -8px 20px 9px" }}>
                 <img src={pin} style={{ opacity: (dragging ? 0.5 : 1), width: "50px", top: "-15px", alignSelf: "center", marginLeft: "25px", position: "absolute" }} />
-                <div ref={dragRef} onDragStart={handleDragLocal} onDragEnd={handleDragLocal} style={{ width: "100%", height: "100%", backgroundColor: color }}>
+                <div ref={dragRef} onDragStart={handleDragLocal} onDragEnd={handleDragLocal} style={{ width: "100%", height: "100%", backgroundColor: color, transitionDuration: "unset" }}>
                     <div style={{ height: "80%", margin: "15% 5% 5%" }}>
                         <NoteText entry={entry} />
                     </div>
