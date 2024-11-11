3a1950587403d200e85e9193d627d82e0259e5b4
Skyteks <Schattenpalkia@googlemail.com>
2024-11-11 11:47:39 +0100

    Update user model

--- src/models/note.model.js
+++ src/models/note.model.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const {colors} = require("../data/colors.json");
+console.log("COLORS: ", colors);
 
 const noteSchema = new Schema({
     title: { type: String, required: true, unique: true },
-    status: { type: String,  required: true },
-    color: { type: String,  required: true },
+    status: { type: String, enum: ["backlog", "todo", "doing", "test", "done"], required: true },
+    color: { type: String, enum: colors, required: true },
     text1: { type: String, required: true },
     text2: { type: String },
     text3: { type: String },
