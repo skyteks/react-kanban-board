b57b4bba127c6d0b329e569553081d7e70002a92
Skyteks <Schattenpalkia@googlemail.com>
2024-11-22 12:50:49 +0100

    Add environment variables

--- src/axiosAPI.js
+++ src/axiosAPI.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { getStatusMeaning } from "./HelperFunctions";
 
-const API_URI = "http://localhost:3000";
+const backendUri = (import.meta.env.VITE_BACKEND_URI || "http://localhost") + (":" + import.meta.env.VITE_BACKEND_PORT ||":3000");
+console.log(backendUri);
+
 
 async function postUser(uriPath, requestBody) {
     const what = uriPath.substring(1).toUpperCase();
@@ -9,7 +11,7 @@ async function postUser(uriPath, requestBody) {
         console.log(what, "Reqest Body has empty values");
         return false;
     }
-    const uri = API_URI + uriPath;
+    const uri = backendUri + uriPath;
     console.log("POST", uri, requestBody);
 
     return await axios.post(uri, requestBody)
@@ -22,7 +24,7 @@ async function postUser(uriPath, requestBody) {
         .catch((error) => {
             console.log(error);
             
-            const responseMessage = error.response.data.message ? error.response.data.message : getStatusMeaning(error.status)[0];
+            const responseMessage = error?.response?.data?.message ? error.response.data.message : getStatusMeaning(error.status)[0];
             console.log(what, responseMessage);
 
             return { statusCode: error.status, message: responseMessage, success: false };
@@ -32,7 +34,7 @@ async function postUser(uriPath, requestBody) {
 async function getUser(uriPath, token) {
     const what = uriPath.substring(1).toUpperCase();
     const header = getTokenHeader(token);
-    const uri = API_URI + uriPath;
+    const uri = backendUri + uriPath;
     console.log("GET", uri);
 
     return await axios.get(uri, header)
@@ -43,7 +45,7 @@ async function getUser(uriPath, token) {
             return { data: response.data, statusCode: response.status, message: responseMessage, success: true };
         })
         .catch((error) => {
-            const responseMessage = error.response.data.message ? error.response.data.message : getStatusMeaning(error.status)[0];
+            const responseMessage = error?.response?.data?.message ? error.response.data.message : getStatusMeaning(error.status)[0];
             console.log(what, responseMessage);
 
             return { statusCode: error.status, message: responseMessage, success: false };
@@ -52,7 +54,7 @@ async function getUser(uriPath, token) {
 
 async function getUsernames(token) {
     const header = getTokenHeader(token);
-    const uri = API_URI + "/mongo/users";
+    const uri = backendUri + "/mongo/users";
     console.log("GET", uri);
 
     return await axios.get(uri, header)
@@ -63,7 +65,7 @@ async function getUsernames(token) {
             return { data: response.data, statusCode: response.status, message: responseMessage, success: true };
         })
         .catch((error) => {
-            const responseMessage = error.response.data.message ? error.response.data.message : getStatusMeaning(error.status)[0];
+            const responseMessage = error?.response?.data?.message ? error.response.data.message : getStatusMeaning(error.status)[0];
             console.log("MONGO", responseMessage);
 
             return { statusCode: error.status, message: responseMessage, success: false };
@@ -72,7 +74,7 @@ async function getUsernames(token) {
 
 async function getNotes(token) {
     const header = getTokenHeader(token);
-    const uri = API_URI + "/mongo/notes";
+    const uri = backendUri + "/mongo/notes";
     console.log("GET", uri);
 
     return await axios.get(uri, header)
@@ -83,7 +85,7 @@ async function getNotes(token) {
             return { data: response.data, statusCode: response.status, message: responseMessage, success: true };
         })
         .catch((error) => {
-            const responseMessage = error.response.data.message ? error.response.data.message : getStatusMeaning(error.status)[0];
+            const responseMessage = error?.response?.data?.message ? error.response.data.message : getStatusMeaning(error.status)[0];
             console.log("MONGO", responseMessage);
 
             return { statusCode: error.status, message: responseMessage, success: false };
@@ -92,7 +94,7 @@ async function getNotes(token) {
 
 async function patchNote(requestBody, token) {
     const header = getTokenHeader(token);
-    const uri = API_URI + "/mongo/notes/" + requestBody.data._id;
+    const uri = backendUri + "/mongo/notes/" + requestBody.data._id;
     console.log("PATCH", uri, requestBody);
 
     return await axios.patch(uri, requestBody, header)
@@ -103,7 +105,7 @@ async function patchNote(requestBody, token) {
             return { data: response.data, statusCode: response.status, message: responseMessage, success: true };
         })
         .catch((error) => {
-            const responseMessage = error.response.data.message ? error.response.data.message : getStatusMeaning(error.status)[0];
+            const responseMessage = error?.response?.data?.message ? error.response.data.message : getStatusMeaning(error.status)[0];
             console.log("PATCH", responseMessage);
 
             return { statusCode: error.status, message: responseMessage, success: false };
@@ -112,7 +114,7 @@ async function patchNote(requestBody, token) {
 
 async function postNote(requestBody, token) {
     const header = getTokenHeader(token);
-    const uri = API_URI + "/mongo/notes";
+    const uri = backendUri + "/mongo/notes";
     console.log("POST", uri, requestBody);
 
     return await axios.post(uri, requestBody, header)
@@ -123,7 +125,7 @@ async function postNote(requestBody, token) {
             return { data: response.data, statusCode: response.status, message: responseMessage, success: true };
         })
         .catch((error) => {
-            const responseMessage = error.response.data.message ? error.response.data.message : getStatusMeaning(error.status)[0];
+            const responseMessage = error?.response?.data?.message ? error.response.data.message : getStatusMeaning(error.status)[0];
             console.log("PATCH", responseMessage);
 
             return { statusCode: error.status, message: responseMessage, success: false };
